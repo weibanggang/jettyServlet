6dec371427ddeb557be7da3bda1571266d052c82
weibanggang <761273133@qq.com>
2018-08-09 08:51:56 +0800

    20180809

---
+++ src/com/MyServlet.java
@@ -0,0 +1,14 @@
+package com;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+@WebServlet("/MyServlet")
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().print("我是Myservlet");
+    }
+}
